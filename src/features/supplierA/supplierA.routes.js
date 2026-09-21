a8cf02719e7663a2f4a2d8dcd191030c2a665e5e
rahulrkumar2023 <69042826+rahulrkumar2023@users.noreply.github.com>
2026-09-21 22:48:09 +0530

    Add files via upload

--- src/features/supplierA/supplierA.routes.js
+++ src/features/supplierA/supplierA.routes.js
@@ -3,6 +3,7 @@ import HotelController from "./hotels.controller";
 
 const supplierARouter = express.Router();
 const hotelController = new HotelController();
+supplierARouter.get("/hotels" , (req , res)=>hotelController.filterHotels(req,res))
 
 
 
